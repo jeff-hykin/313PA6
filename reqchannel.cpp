3ba28ddba625000d11b73882b53e6d7a5b62708d
Jeff Hykin <jeffhykin@Jeffs-MacBook-Pro-2.local>
2018-11-21 18:31:46 -0600

    reqchannel is now a pure virtual function

--- reqchannel.cpp
+++ reqchannel.cpp
@@ -5,137 +5,16 @@
             Department of Computer Science
             Texas A&M University
     Date  : 2012/07/11
-
 */
 
 #include "reqchannel.h"
-#include <cassert>
-#include <cstring>
-#include <errno.h>
-#include <fcntl.h>
-#include <iostream>
-#include <signal.h>
-#include <stdio.h>
-#include <stdlib.h>
-#include <sys/stat.h>
-#include <sys/types.h>
-#include <unistd.h>
-
-using namespace std;
-
-#define MAX_MESSAGE 255
-
 // 
 // Constructors
 // 
-    RequestChannel::RequestChannel()
-        {
-        }
-    RequestChannel::RequestChannel(const std::string _name, const Side _side) : my_name(_name), my_side(_side), side_name((_side == RequestChannel::SERVER_SIDE) ? "SERVER" : "CLIENT")
-        {
-        }
-    
+    // code breaks if you remove this 
+    // it shouldnt because its a pure vitural function
+    // it shouldnt because it literally does nothing
+    // but it breaks anyways ¯\_(ツ)_/¯
     RequestChannel::~RequestChannel()
         {
         }
-
-// 
-// Methods
-// 
-    std::string RequestChannel::pipe_name(Mode _mode)
-        {
-            std::string pname = "fifo_" + my_name;
-
-            if(my_side == CLIENT_SIDE)
-                {
-                    if(_mode == READ_MODE)
-                        pname += "1";
-                    else
-                        pname += "2";
-                }
-            else
-                {
-                    /* SERVER_SIDE */
-                    if(_mode == READ_MODE)
-                        pname += "2";
-                    else
-                        pname += "1";
-                }
-            return pname;
-        }
-    void RequestChannel::create_pipe(string _pipe_name)
-        {
-            mkfifo(_pipe_name.c_str(), 0600) < 0;
-        }
-
-    void RequestChannel::open_write_pipe(string _pipe_name)
-        {
-            create_pipe(_pipe_name);
-            write_file_descriptor = open(_pipe_name.c_str(), O_WRONLY);
-            if(write_file_descriptor < 0)
-                {
-                    cout << "Error opening new pipe("<< _pipe_name<< ") inside of open_write_pipe" << "\n";
-                }
-        }
-
-    void RequestChannel::open_read_pipe(string _pipe_name)
-        {
-            create_pipe(_pipe_name);
-            read_file_descriptor = open(_pipe_name.c_str(), O_RDONLY);
-            cout << "read_file_descriptor = " << (read_file_descriptor) << "\n";
-            if(read_file_descriptor < 0)
-                {
-                    perror("");
-                    exit(0);
-                }
-        }
-
-    string RequestChannel::cread()
-        {
-            // summary:
-                // /* Blocking read of data from the channel. Returns a string of characters
-                // read from the channel. Returns NULL if read failed. */
-            char buf[MAX_MESSAGE];
-            cout << "read_file_descriptor = " << (read_file_descriptor) << "\n";
-            cout << "buf = " << (buf) << "\n";
-            cout << "MAX_MESSAGE = " << (MAX_MESSAGE) << "\n";
-            if(read(read_file_descriptor, buf, MAX_MESSAGE) <= 0)
-                {
-                    cerr << "Error in RequestChannel cread()" << "\n";
-                }
-            string s = buf;
-            return s;
-        }
-
-    void RequestChannel::cwrite(string msg)
-        {
-            if(msg.size() > MAX_MESSAGE)
-                {
-                    cerr << "Error in cwrite()" << "\n";
-                }
-            // msg.size() + 1 to include the NULL byte
-            if(write(write_file_descriptor, msg.c_str(), msg.size() + 1) < 0)
-                { 
-                    cerr << "Error in cwrite()" << "\n";
-                }
-        }
-
-    std::string RequestChannel::name()
-        {
-            /* Returns the name of the request channel. */
-            return my_name;
-        }
-
-    int RequestChannel::read_fd()
-        {
-            /* Returns the file descriptor used to read from the channel. */
-            return read_file_descriptor;
-        }
-
-    int RequestChannel::write_fd()
-        {
-            /* Returns the file descriptor used to write to the channel. */
-            return write_file_descriptor;
-        }
-
-#undef MAX_MESSAGE
--- reqchannel.h
+++ reqchannel.h
@@ -33,21 +33,18 @@ class RequestChannel
             int write_file_descriptor;
             int read_file_descriptor;
         // methods
-            virtual string pipe_name(Mode _mode);
-            virtual void   create_pipe(string _pipe_name);
-            virtual void   open_read_pipe(string _pipe_name);
-            virtual void   open_write_pipe(string _pipe_name);
+            virtual string pipe_name(Mode _mode) = 0;
+            virtual void   create_pipe(string _pipe_name) = 0;
+            virtual void   open_read_pipe(string _pipe_name) = 0;
+            virtual void   open_write_pipe(string _pipe_name) = 0;
 
-    
         // Constructors
-            RequestChannel();
-            RequestChannel(const string _name, const Side _side);
-            virtual ~RequestChannel();
+            virtual ~RequestChannel() = 0;
         // Methods
-            virtual string cread();
-            virtual void   cwrite(string _msg);
-            virtual string name();
-            virtual int    read_fd();
-            virtual int    write_fd();
+            virtual string cread() = 0;
+            virtual void   cwrite(string _msg) = 0;
+            virtual string name() = 0;
+            virtual int    read_fd() = 0;
+            virtual int    write_fd() = 0;
     };
 #endif
